6eeb79ea5c71139b431052ea2e57d62a7d9546c5
machachi25 <114490748+machachi25@users.noreply.github.com>
2023-11-21 16:34:36 -0500

    Update Attendees.java

--- Attendees.java
+++ Attendees.java
@@ -1,11 +1,43 @@
 import java.util.*;
 public class Attendees
 {
-	private int ID;
-	private String Name;
-	private int companyName;
+	private int iD;
+	private String name;
+	private int companyID;
 	
-	public Attendees(String guest)
-	{}
-}
+	public Attendees(int newID, String newName, int newCompanyID) //constructor
+	{
+			iD = newID;
+			name = newName;
+			companyID = newCompanyID;
+	}
+	
+	//getters and setters
+	public void setID(int newID)  {//updates the new ID into the original object 
+		iD = newID;
+	}
+	public void setName(String newName)  {//same thing but with name and company ID
+		name = newName;
+		}
+	public void setCompany(int newCompanyID) {
+		companyID = newCompanyID;
+	}
+	    //getters
+		public int getID()  {//
+		return iD;
+	}
+	public String setName()  {//
+		return name;
+		}
+	public int setCompany() {
+		return companyID;
+	}
+	
+	
+	public String toString()
+	{
+		return(name + " has an ID of: " + iD + " and works for " + companyID + " .");
+	}
 		
+}
+
